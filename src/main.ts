559b817043ef6cc44c0db898f3bf70adeafffa05
Ariandi Nugraha <dbduabelas@gmail.com>
2019-08-17 10:02:51 +0700

    first Commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import jQuery from "jquery";
+import "popper.js";
+import "bootstrap";
+import "./assets/app.scss";
+
+declare global {
+  interface Window { 
+      $:any; 
+      jQuery:any; 
+  }
+}
+
+window.$ = window.jQuery = jQuery;
 
 Vue.config.productionTip = false;
+
+Vue.component("Navbar", require("./components/NavBar.vue").default);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Navbar></Navbar>
+    <Hero></Hero>
+    <products-list></products-list>
+
+    <div class="container h-100">
+      <div class="row p-5 h-100 justify-content-center align-items-center">
+        <div class="col-md-6">
+          <img src="/img/svg/product.svg" alt="Products" width="400" />
+        </div>
+        <div class="col-md-6">
+          <h3>Learn More About us</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quaerat voluptate iusto? Nihil voluptas vitae veritatis magnam reprehenderit, reiciendis ex? Libero quasi natus veniam voluptatum, voluptas exercitationem ratione consectetur et!
+          </p>
+          <button class="btn custom-btn btn-outline-primary mr-4">
+            Browse Products
+          </button>
+        </div>
+      </div>
+    </div>
+    <login></login>
+
+    <mini-cart></mini-cart>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Hero from "@/components/Hero.vue";
+import Products from "@/sections/Products.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    Hero
   }
 })
 export default class Home extends Vue {}
